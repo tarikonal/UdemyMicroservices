a67ec60f07fac970405687fd619b48760cd51b28
3817350 <3817350@KHYGDYVT23>
2021-08-13 17:48:54 +0300

    Add project files.

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Services/ICategoryService.cs
@@ -0,0 +1,18 @@
+﻿using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Models;
+using FreeCourse.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Catalog.Services
+{
+    interface ICategoryService
+    {
+         Task<Response<List<CategoryDto>>> GetAllAsync();
+         Task<Response<CategoryDto>> CreateAsync(Category category);
+         Task<Response<CategoryDto>> GetByIdAsync(String id);
+
+    }
+}
---
+++ Shared/FreeCourse.Shared/Dtos/Response.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace FreeCourse.Shared.Dtos
+{
+    public class Response<T>
+    {
+        public T Data { get; private set; }
+
+        [JsonIgnore]
+        public int StatusCode { get; private set; }
+        [JsonIgnore]
+        public bool IsSuccessfull { get; set; }
+        public List<string> Errors { get; set; }
+
+        public static Response<T> Success(T data, int statusCode)
+        {
+            return new Response<T> { Data = data, StatusCode = statusCode, IsSuccessfull = true };
+        }
+
+        public static Response<T> Succes(int statusCode)
+        {
+            return new Response<T> { Data = default(T), StatusCode = statusCode, IsSuccessfull = true };
+        }
+
+        public static Response<T> Fail(List<string> errors, int statusCode)
+        {
+            
+            return new Response<T> {
+                Errors = errors,
+                StatusCode = statusCode,
+                IsSuccessfull = false 
+            };
+        }
+
+        public static Response<T> Fail(string error, int statusCode) 
+        {
+            return new Response<T> { 
+                StatusCode = statusCode, 
+                Errors = new List<string> { error }, 
+                IsSuccessfull = false 
+            };
+        }
+    }
+}
